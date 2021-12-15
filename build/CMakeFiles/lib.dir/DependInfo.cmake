090d1f2861330711b7d5b16d22b7790ef28652e5
denis <avramdenis58@gmail.com>
2021-12-15 21:48:13 +0200

    Create CMakeList and build directory.

---
+++ build/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/denisubuntu/Desktop/IEP/IEP/src/AdvancePrinter.cpp" "/home/denisubuntu/Desktop/IEP/IEP/build/CMakeFiles/lib.dir/src/AdvancePrinter.cpp.o"
+  "/home/denisubuntu/Desktop/IEP/IEP/src/NormalPrinter.cpp" "/home/denisubuntu/Desktop/IEP/IEP/build/CMakeFiles/lib.dir/src/NormalPrinter.cpp.o"
+  "/home/denisubuntu/Desktop/IEP/IEP/src/Person.cpp" "/home/denisubuntu/Desktop/IEP/IEP/build/CMakeFiles/lib.dir/src/Person.cpp.o"
+  "/home/denisubuntu/Desktop/IEP/IEP/src/Socket.cpp" "/home/denisubuntu/Desktop/IEP/IEP/build/CMakeFiles/lib.dir/src/Socket.cpp.o"
+  "/home/denisubuntu/Desktop/IEP/IEP/src/SuperPrinter.cpp" "/home/denisubuntu/Desktop/IEP/IEP/build/CMakeFiles/lib.dir/src/SuperPrinter.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/src/AdvancePrinter.cpp.o"
+  "CMakeFiles/lib.dir/src/NormalPrinter.cpp.o"
+  "CMakeFiles/lib.dir/src/Person.cpp.o"
+  "CMakeFiles/lib.dir/src/Socket.cpp.o"
+  "CMakeFiles/lib.dir/src/SuperPrinter.cpp.o"
+  "liblib.a"
+  "liblib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
